6458dcdc6b160e05e592e628762504338a7ce857
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2024-01-22 23:02:26 -0300

    add fontawesome lib from icons

--- personal_expenses/settings.py
+++ personal_expenses/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "fontawesomefree",
     "apps.bills",
     "apps.cards",
     "apps.agreements",
